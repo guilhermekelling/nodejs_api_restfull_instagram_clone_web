d35c310971c48305cb501f46e0501b2d8eb9ee63
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2020-06-12 14:45:06 -0300

    Method Post: incluindo documentos no MongoDB

--- api/server.js
+++ api/server.js
@@ -12,8 +12,32 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost',27017,{}),
+    {}
+);
+
 console.log('Servidor HTTP esta escutando na porta ' + port);
 
 app.get('/', function(req, res){
 	res.send({msg:'Olá'});
+});
+
+//URI + verbo HTTP
+app.post('/api', function(req,res){
+	var dados = req.body;
+
+	db.open( function(err, mongoclient){
+		mongoclient.collection('postagens', function(err, collection){
+			collection.insert(dados, function(err, records){
+				if(err){
+					res.json({'status' : 'erro'});
+				} else{
+					res.json({'status' : 'inclusão realizada com sucesso'});
+				}
+				mongoclient.close();
+			});
+		});
+	});
 });
