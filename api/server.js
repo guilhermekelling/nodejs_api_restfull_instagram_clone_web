680dbdb799fa3f8bfd40779816b1e862ee04a1db
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2020-06-16 17:08:09 -0300

    Incluindo publicações, configurando o Connect-Multiparty, exibindo postagens na timeline, exibindo imagens

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    multiparty = require('connect-multiparty'),
     mongodb = require('mongodb'),
-    objectId = require('mongodb').ObjectID;
+    objectId = require('mongodb').ObjectID,
+    fs = require('fs');
 
 var app = express();
 
 // body-parser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 var port = 8080;
 
@@ -28,24 +31,50 @@ app.get('/', function(req, res){
 //URI + verbo HTTP
 //POST (create)
 app.post('/api', function(req,res){
-	var dados = req.body;
 
-	db.open( function(err, mongoclient){
-		mongoclient.collection('postagens', function(err, collection){
-			collection.insert(dados, function(err, records){
-				if(err){
-					res.json({'status' : 'erro'});
-				} else{
-					res.json({'status' : 'inclusão realizada com sucesso'});
-				}
-				mongoclient.close();
-			});
-		});
-	});
+    res.setHeader("Access-Control-Allow-Origin","*");
+
+    var date = new Date();
+    time_stamp = date.getTime();
+
+    var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
+	
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+
+    fs.rename(path_origem,path_destino, function(err){
+        if(err){
+            res.status(500).json({error: err});
+            return;
+        }
+
+    var dados = {
+        url_imagem : url_imagem,
+        titulo: req.body.titulo
+    }
+
+    db.open( function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.insert(dados, function(err, records){
+                    if(err){
+                        res.json({'status' : 'erro'});
+                    } else{
+                        res.json({'status' : 'inclusão realizada com sucesso'});
+                    }
+                    mongoclient.close();
+                });
+            });
+        });
+
+    });
+
 });
 
 //GET (ready)
 app.get('/api', function(req, res){
+
+    res.setHeader("Access-Control-Allow-Origin","*");
+
     db.open(function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
             collection.find().toArray(function(err, results){
@@ -73,6 +102,21 @@ app.get('/api/:id', function(req, res){
                 mongoclient.close();
             });
         });
+    });
+});
+
+app.get('/imagens/:imagem', function(req, res){
+    var img = req.params.imagem;
+
+    fs.readFile('./uploads/' + img, function(err, content){
+        if(err){
+            res.status(400).json({ err });
+            return;
+        }
+
+        res.writeHead(200, { 'content-type' : 'image/jpg' });
+        res.end(content);
+
     });
 });
 
